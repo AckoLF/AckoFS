4bf1d330ead70ace531153b1ec058cd968003f61
Al3kSaNdaR <aleksandar.ivanovic.94@gmail.com>
2016-06-23 02:10:32 +0200

    42

--- AckoFS/AckoFS/KernelFile.cpp
+++ AckoFS/AckoFS/KernelFile.cpp
@@ -6,3 +6,13 @@ KernelFile::KernelFile() {
 
 KernelFile::~KernelFile() {
 }
+
+char KernelFile::seek(BytesCnt position) {
+	// TODO(acko): Check if position is legal 
+	this->position = position;
+	return '1';
+}
+
+BytesCnt KernelFile::filePos() {
+	return position;
+}
